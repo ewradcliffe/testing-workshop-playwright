937f8c1855b181c8cfc623a2b18139d362248c65
Cameron Latham <cameron.latham@solirius.com>
2025-03-25 13:41:22 +0000

    formatted tests

--- tests/irregular_hours.spec.ts
+++ tests/irregular_hours.spec.ts
@@ -1,21 +1,24 @@
 import {expect, test} from "@playwright/test";
 
-test(`Radio Buttons Visible`, async ({ page }) => {
+test(`Radio Buttons Are Visible`, async ({ page }) => {
     await page.goto('https://www.gov.uk/calculate-your-holiday-entitlement/y');
+
     // Ensure buttons are visible
     await expect(page.locator(`div.govuk-radios`))
         .toBeVisible(); 
 });
 
-test(`Header Visible`, async ({ page }) => {
+test(`Header Is Visible`, async ({ page }) => {
     await page.goto('https://www.gov.uk/calculate-your-holiday-entitlement/y');
+
     // Ensure header is visible
     await expect(page.locator(`h1.govuk-fieldset__heading`))
         .toBeVisible(); 
 });
 
-test(`Continue Button Visible`, async ({ page }) => {
+test(`Continue Button Is Visible`, async ({ page }) => {
     await page.goto('https://www.gov.uk/calculate-your-holiday-entitlement/y');
+
     // Ensure button is visible
     await expect(page.getByRole('button', {name: 'Continue'}))
         .toBeVisible();
@@ -35,6 +38,7 @@ test(`Radio Buttons Start Unchecked`, async ({ page }) => {
 
 test(`Radio Buttons Check When Clicked`, async ({ page }) => {
     await page.goto('https://www.gov.uk/calculate-your-holiday-entitlement/y');
+
     // Check No leaves Yes unchecked
     await Promise.all([
         // Tick "No" Button
@@ -47,14 +51,14 @@ test(`Radio Buttons Check When Clicked`, async ({ page }) => {
             .toBeChecked(),      
     ]);
     
-    // Check the Yes funtionality
+    // Check the Yes leaves No unchecked
     await Promise.all([
         // Check Button
         page.getByLabel('Yes').check(),
         // Ensure Yes button is checked
         expect(page.getByLabel('Yes'))
             .toBeChecked(),
-        // Ensure while yes checked, no uncheked
+        // Ensure while Yes checked, No uncheked
         expect(page.getByLabel('No'))
             .not.toBeChecked(),
     ]);
@@ -62,35 +66,39 @@ test(`Radio Buttons Check When Clicked`, async ({ page }) => {
 
 test(`Radio Button Unchecks Other Button`, async ({ page }) => {
     await page.goto('https://www.gov.uk/calculate-your-holiday-entitlement/y');
+
+    // Check Yes box
     await page.getByLabel('Yes').check();
+    // Assert No is unchecked
     await expect(page.getByLabel('No'))
         .not.toBeChecked();
+    // Now check No box
     await page.getByLabel('No').check();
+    // And assert Yes is now unchkecked
     await expect(page.getByLabel('Yes'))
         .not.toBeChecked();
 });
 
 test(`Irregular Hours Route`, async ({ page }) => {
     await page.goto("https://www.gov.uk/calculate-your-holiday-entitlement/y");
-    await page.getByLabel('Yes').check();
 
+    // Check Yes box
+    await page.getByLabel('Yes').check();
     await Promise.all([
-        // Call waitForNavigation before clicking, to set up waiting.
         page.waitForURL("https://www.gov.uk/calculate-your-holiday-entitlement/y/irregular-hours-and-part-year"),
-        // Click button
+        // Click Continue button
         page.getByRole('button', {name: 'Continue'}).click(),
     ]);
 });
 
 test(`Regular Hours Route`, async ({ page }) => {
     await page.goto("https://www.gov.uk/calculate-your-holiday-entitlement/y");
+
+    // Check No box
     await page.getByLabel('No').check();
-    
     await Promise.all([
-        // Call waitForNavigation before clicking, to set up waiting.
         page.waitForURL("https://www.gov.uk/calculate-your-holiday-entitlement/y/regular"),
-        
-        // Click button
+        // Click Continue button
         page.getByRole('button', {name: 'Continue'}).click(),
     ]);
 });
